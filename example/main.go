a2138b7095914ff379defc4faf7dcf9c52a9b1b8
Nico Kahlert <nico.kahlert@outlook.de>
2020-10-03 15:41:56 +0200

    ADD producer and example

--- example/main.go
+++ example/main.go
@@ -3,10 +3,24 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/reactivex/rxgo/v2"
 
 	"github.com/PlantUp/kafkaesque"
+)
+
+const (
+	// ConsumerMode consume a topic
+	ConsumerMode = "consumer"
+	// ProducerMode produce from Observable
+	ProducerMode = "producer"
 )
 
 // BookRequest an example type
@@ -16,22 +30,48 @@ type BookRequest struct {
 }
 
 func main() {
+	mode := flag.String("mode", "consumer", "either in producer or consumer mode")
+	topics := flag.String("topics", "test", "Comma-separeted list of topics")
+	broker := flag.String("broker", "localhost:9092", "Comma-seperated list of brokers")
+	name := flag.String("name", "test", "ClientID")
+	flag.Parse()
+	brokerList := strings.Split(*broker, ",")
+	topicList := strings.Split(*topics, ",")
+	switch *mode {
+	case ConsumerMode:
+		consumeTopic(
+			brokerList,
+			topicList,
+			*name,
+		)
+	case ProducerMode:
+		produceTopic(
+			brokerList,
+			topicList,
+			*name,
+		)
+	default:
+		fmt.Println("Error no such mode")
+		os.Exit(2)
+	}
+}
+
+func consumeTopic(brokers []string, topics []string, clientID string) {
 	testConsumer, err := kafkaesque.NewConsumer(&kafkaesque.Config{
 		AutoOffsetReset:  kafkaesque.AutoOffsetReset.Latest,
-		BootstrapServers: []string{"localhost:9092"},
+		BootstrapServers: brokers,
 		IsolationLevel:   kafkaesque.IsolationLevel.ReadCommitted,
 		MessageMaxBytes:  1048588,
-		ClientID:         "test_1",
-		GroupID:          "test_group",
+		ClientID:         clientID,
+		GroupID:          clientID + "_group",
 		SecurityProtocol: kafkaesque.SecurityProtocol.Plaintext,
 	},
-		"test",
-		"test2",
+		topics...,
 	)
 	if err != nil {
 		panic(err)
 	}
-	log.Println("Showing events:")
+	log.Println(fmt.Sprintf("Showing events for \u001b[34m%s\u001b[0m on \u001b[31m%s\u001b[0m:", topics, brokers))
 	bookRequests := testConsumer.Events.
 		Map(func(_ context.Context, item interface{}) (interface{}, error) {
 			var request BookRequest
@@ -40,6 +80,58 @@ func main() {
 		})
 	defer testConsumer.Close()
 	for event := range bookRequests.Observe() {
-		log.Println(fmt.Sprintf("%T", event.V))
+		bookRequest := event.V.(BookRequest)
+		log.Println(fmt.Sprintf(
+			"Book:\t%s\tPerson:\t%s",
+			bookRequest.Book,
+			bookRequest.Person,
+		))
+	}
+}
+
+func produceTopic(brokers []string, topics []string, clientID string) {
+	events := rxgo.Defer(
+		[]rxgo.Producer{
+			func(_ context.Context, ch chan<- rxgo.Item) {
+				for name, title := range map[string]string{
+					"Pamela Osborne":  "10 Awesome Ways to Photograph Blue Bottles",
+					"Inayah Brown":    "21 Myths About Blue bottles Debunked",
+					"Ikrah Blair":     "How to Make Your Own Vast Hat for less than £5",
+					"Aurora Stafford": "An analysis of handsome jugs",
+					"Hafsa Scott":     "Why Are My Elbows Growing?",
+				} {
+					ch <- rxgo.Of(BookRequest{Person: name, Book: title})
+					time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+				}
+			},
+		}).Map(
+		func(_ context.Context, item interface{}) (interface{}, error) {
+			return func(br BookRequest) ([]byte, error) {
+				return json.Marshal(&br)
+			}(item.(BookRequest))
+		},
+	)
+
+	testProducer, err := kafkaesque.NewProducer(
+		&kafkaesque.Config{
+			AutoOffsetReset:  kafkaesque.AutoOffsetReset.Latest,
+			BootstrapServers: brokers,
+			IsolationLevel:   kafkaesque.IsolationLevel.ReadCommitted,
+			MessageMaxBytes:  1048588,
+			ClientID:         clientID,
+			GroupID:          clientID + "_group",
+			SecurityProtocol: kafkaesque.SecurityProtocol.Plaintext,
+		},
+		topics[0],
+		[]byte("BookRequests"),
+		events,
+	)
+
+	defer testProducer.Close()
+	if err != nil {
+		panic(err)
+	}
+	for info := range testProducer.GetInfos().Observe() {
+		fmt.Println(info)
 	}
 }
